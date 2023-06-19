5bc9f53ebfe107e32d510cb336cd6d8ccc84e34a
Ajokotonyepeter <ajokotonyepeter@gmail.com>
2023-06-19 12:14:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various types on the computer
- * Return 0 (Success)
+ * main - A program that prints description values
+ * Return: 0 (successful)
  */
 int main(void)
 {
